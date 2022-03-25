de1141e6fe406e5734032ac06e605586c46ff028
Mike Maietta <michaelmaietta@upwork.com>
2022-03-25 14:03:32 -0700

    cleanup repro env

--- cypress/integration/hello-world.spec.js
+++ cypress/integration/hello-world.spec.js
@@ -13,33 +13,3 @@ describe('HelloWorld', () => {
     cy.get('h1').should('have.text', msg)
   })
 })
-
-// import { mountCallback } from '@cypress/vue'
-
-// describe('Handling User Input', () => {
-//   // Example from https://vuejs.org/v2/guide/#Handling-User-Input
-//   const template = `
-//     <div>
-//       <p>{{ message }}</p>
-//       <button @click="reverseMessage">Reverse Message</button>
-//     </div>
-//   `
-
-//   function data() {
-//     return { message: 'Hello Vue.js!' }
-//   }
-
-//   const methods = {
-//     reverseMessage: function () {
-//       this.message = this.message.split('').reverse().join('')
-//     },
-//   }
-
-//   beforeEach(mountCallback({ template, data, methods }))
-
-//   it('reverses text', () => {
-//     cy.contains('Hello Vue')
-//     cy.get('button').click()
-//     cy.contains('!sj.euV olleH')
-//   })
-// })
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path'
 import { Configuration as WebpackConfiguration } from 'webpack'
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
 
@@ -12,16 +11,6 @@ interface Configuration extends WebpackConfiguration {
 export function commonConfig (): Configuration {
   const mode = 'development'
   return {
-    // devServer: {
-    //   port: 9999,
-    //   host: "0.0.0.0",
-    //   static: __dirname,
-    //   // hot: true,
-    //   // https: {
-    //   //   cert: "./cert.crt",
-    //   //   key: "./key.pem"
-    //   // }
-    // } as WebpackDevServerConfiguration,
     module: {
       rules: [
         {
@@ -56,52 +45,4 @@ export function commonConfig (): Configuration {
       new VueLoaderPlugin()
     ]
   };
-}
-
-const sassLoaders = (debugMode: boolean, cssOptions: any) => {
-  return [
-    {
-      loader: 'css-loader',
-      options: {
-        importLoaders: 2,
-        esModule: false,
-        ...cssOptions
-      }
-    },
-    {
-      loader: 'postcss-loader',
-      options: {
-        postcssOptions: {
-          config: resolve(__dirname, "node_modules/@upwork/styles-base/postcss.config.js"),
-        },
-      }
-    },
-    'resolve-url-loader',
-    {
-      loader: 'sass-loader',
-      options: {
-        sourceMap: true, // `true` needed for `resolve-url-loader`
-        sassOptions: {
-          compass: true,
-          bundleExec: true,
-          style: debugMode ? 'expanded' : 'compressed',
-          sourceComments: debugMode,
-          includePaths: [
-            'redux/src',
-            'redux/src/view',
-            'node_modules/',
-            'node_modules/@upwork/styles-base',
-            'node_modules/@upwork/fonts-global/src',
-            'node_modules/@upwork/ui-components/src/styles/globals',
-            'node_modules/@upwork/ui-components/src/styles/assets',
-            'node_modules/@upwork/ui-components/src/styles/themes',
-            'styles/sass',
-            'assets',
-            'redux/assets/images',
-            'node_modules/compass-mixins/lib'
-          ].map(item => resolve(__dirname, item))
-        }
-      }
-    }
-  ]
 }
